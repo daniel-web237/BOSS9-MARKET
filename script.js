417b7ae49b58b3e6de8fa51d4d108f1424423cae
daniel-web237 <lamachinebedimo@gmail.com>
2026-08-06 10:48:17 +0100

    Remove mobile menu toggle and update initialization
    Removed mobile menu toggle functionality and adjusted initMobileMenu call.

--- script.js
+++ script.js
@@ -228,15 +228,7 @@ function initMobileMenu() {
     links.classList.toggle("open");
   });
 }
-// MENU MOBILE TOGGLE
-const menuToggle = document.getElementById("menuToggle");
-const menuLinks = document.getElementById("menuLinks");
 
-if (menuToggle) {
-  menuToggle.addEventListener("click", () => {
-    menuLinks.classList.toggle("open");
-  });
-}
 
 // ================= LOGOUT =================
 window.logout = function () {
